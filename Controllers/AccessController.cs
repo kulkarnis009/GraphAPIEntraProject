0bddbff8f1f9bdeee6abe8317f059aae8f146444
skulkarni <skulkarni@birg.unbc.ca>
2025-01-08 22:25:01 -0800

    entra to app call

--- Controllers/AccessController.cs
+++ Controllers/AccessController.cs
@@ -1,3 +1,4 @@
+using System.IdentityModel.Tokens.Jwt;
 using EntraGraphAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,33 @@ namespace EntraGraphAPI.Controllers
             bool isAllowed = _accessDecisionService.DecideAccess(role, docType, actions);
             return Ok(new { Role = role, DocumentType = docType, Action = actions, Access = isAllowed });
         }
+
+        [HttpPost("authorize")]
+    public IActionResult Authorize([FromForm] string id_token)
+    {
+        if (string.IsNullOrEmpty(id_token))
+        {
+            return BadRequest(new { error = "The id_token field is required." });
+        }
+
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.ReadJwtToken(id_token);
+
+        // Extract User Object ID (oid)
+        var userId = token.Claims.FirstOrDefault(c => c.Type == "oid")?.Value;
+
+        // Extract App ID (aud)
+        var appId = token.Claims.FirstOrDefault(c => c.Type == "aud")?.Value;
+
+        if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(appId))
+        {
+            return BadRequest(new { error = "User ID or App ID is missing." });
+        }
+
+        // Return User ID and App ID
+        return Ok(new { userId, appId });
+    }
+
     }
 
 }
